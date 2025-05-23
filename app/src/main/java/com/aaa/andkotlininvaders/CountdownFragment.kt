8b10d94361825aad5ffe2db63d1582090a051db9
aaaa1597 <tokushi2011@gmail.com>
2025-05-24 04:41:20 +0900

    ゲーム開始前のカウントダウン画面作成

---
+++ app/src/main/java/com/aaa/andkotlininvaders/CountdownFragment.kt
@@ -0,0 +1,57 @@
+package com.aaa.andkotlininvaders
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import com.aaa.andkotlininvaders.databinding.FragmentCountdownBinding
+import com.aaa.andkotlininvaders.databinding.FragmentMainMenuBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class CountdownFragment : Fragment() {
+    private var _level: Int = 0
+    private lateinit var _binding: FragmentCountdownBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            _level = it.getInt("LEVEL",0)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
+        _binding = FragmentCountdownBinding.inflate(inflater, container, false)
+        _binding.txtCountdownTitle.text = getString(R.string.title_countdown, _level)
+        return _binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        lifecycleScope.launch{
+            /* カウントダウン3秒実行 */
+            (3 downTo 1).forEach {
+                _binding.txtCountdown.text = it.toString()
+                delay(1000)
+            }
+            /* 完了後、ゲーム画面に遷移 */
+//            findNavController().navigate(R.id.action_to_countdown_zoom)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(level: Int) =
+            CountdownFragment().apply {
+                arguments = Bundle().apply {
+                    putInt("LEVEL", level)
+                }
+            }
+    }
+}
--- app/src/main/java/com/aaa/andkotlininvaders/MainMenuFragment.kt
+++ app/src/main/java/com/aaa/andkotlininvaders/MainMenuFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import com.aaa.andkotlininvaders.databinding.FragmentMainMenuBinding
@@ -29,7 +30,8 @@ class MainMenuFragment : Fragment() {
 
         /* ゲーム開始 */
         _binding.btnStart.setOnClickListener {
-
+            val bundle = bundleOf("LEVEL" to 1)
+            findNavController().navigate(R.id.action_to_countdown_zoom, bundle)
         }
 
         /* スコア画面 */
