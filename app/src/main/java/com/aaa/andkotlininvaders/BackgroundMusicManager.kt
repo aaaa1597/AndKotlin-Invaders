a4d2c7a98798a24fe6966ac6ace8ff6f163d4168
aaaa1597 <tokushi2011@gmail.com>
2025-06-28 22:52:38 +0900

    不要ログ削除

--- app/src/main/java/com/aaa/andkotlininvaders/BackgroundMusicManager.kt
+++ app/src/main/java/com/aaa/andkotlininvaders/BackgroundMusicManager.kt
@@ -2,7 +2,6 @@ package com.aaa.andkotlininvaders
 
 import android.content.Context
 import android.media.MediaPlayer
-import android.util.Log
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 
@@ -24,32 +23,20 @@ class BackgroundMusicManager(private val context: Context): DefaultLifecycleObse
         mediaPlayer = MediaPlayer.create(context, R.raw.maou_bgm_8bit17).apply {
             isLooping = true
         }
-        Throwable().stackTrace[0].let {
-            Log.d("aaaaa", "aaaaa ${mediaPlayer}  at ${it.className}.${it.methodName}(${it.fileName}:${it.lineNumber})")
-        }
     }
 
     override fun onResume(owner: LifecycleOwner) {
         super.onResume(owner)
         mediaPlayer?.start()
-        Throwable().stackTrace[0].let {
-            Log.d("aaaaa", "aaaaa ${mediaPlayer}  at ${it.className}.${it.methodName}(${it.fileName}:${it.lineNumber})")
-        }
     }
 
     override fun onPause(owner: LifecycleOwner) {
         super.onPause(owner)
         mediaPlayer?.pause()
-        Throwable().stackTrace[0].let {
-            Log.d("aaaaa", "aaaaa ${mediaPlayer}  at ${it.className}.${it.methodName}(${it.fileName}:${it.lineNumber})")
-        }
     }
 
     override fun onDestroy(owner: LifecycleOwner) {
         super.onDestroy(owner)
         mediaPlayer?.release()
-        Throwable().stackTrace[0].let {
-            Log.d("aaaaa", "aaaaa ${mediaPlayer}  at ${it.className}.${it.methodName}(${it.fileName}:${it.lineNumber})")
-        }
     }
 }
