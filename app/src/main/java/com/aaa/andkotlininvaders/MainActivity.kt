12bc96289eb7446744ceefa2d038edb9a8ff2819
aaaa1597 <tokushi2011@gmail.com>
2025-06-28 19:30:13 +0900

    クリアメッセージ画面を作成中

--- app/src/main/java/com/aaa/andkotlininvaders/MainActivity.kt
+++ app/src/main/java/com/aaa/andkotlininvaders/MainActivity.kt
@@ -8,14 +8,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
-import androidx.transition.Scene
-import androidx.transition.Transition
-import androidx.transition.TransitionManager
 import com.aaa.andkotlininvaders.databinding.ActivityMainBinding
-import kotlinx.coroutines.Job
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    lateinit var _binding: ActivityMainBinding
     val mainActivityViewModel by lazy {
         ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[MainActivityViewModel::class.java]
     }
@@ -23,8 +19,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -32,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         /* ジェスチャバックを無効化 */
-        binding.root.systemGestureExclusionRects = ArrayList<Rect>().apply {
+        _binding.root.systemGestureExclusionRects = ArrayList<Rect>().apply {
             add(Rect(0, android.R.attr.height - 650, android.R.attr.width, android.R.attr.height - 450))
             add(Rect(0, android.R.attr.height - 450, android.R.attr.width, android.R.attr.height - 250))
             add(Rect(0, android.R.attr.height - 250, android.R.attr.width, android.R.attr.height - 50))
